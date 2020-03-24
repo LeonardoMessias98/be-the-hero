9808e9c9c77ca6d353e311c943f4c5b31a70e0ee
LEONARDO MESSIAS RODRIGUES LOPES <leonardo.lopes11@fatec.sp.gov.br>
2020-03-24 16:38:29 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngControllers');
+const IncidentsController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete)
+
+module.exports = routes;
